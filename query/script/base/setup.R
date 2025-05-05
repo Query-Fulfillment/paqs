40f4af352a7d8a1be23b6431c9b3f268446c5f6b
Sahal Master <sahalmaster@outlook.com>
2025-05-05 08:19:13 -0400

    Moving db type patch in setup

--- query/script/base/setup.R
+++ query/script/base/setup.R
@@ -636,7 +636,6 @@ utils::assignInNamespace(
 }
 
 
-#' Modifications to argos package for extending functionality to other databases
 patch_argos <- function() {
   argos$public_methods$load_codeset <- function(name,
                                                 col_types = "iccc",
@@ -644,6 +643,23 @@ patch_argos <- function() {
                                                 indexes = list("concept_code"),
                                                 full_path = FALSE,
                                                 db = self$config("db_src")) {
+    conn_class <- class(db)[1]
+    method_name <- paste0("load_codeset.", conn_class)
+
+
+    if (method_name %in% names(self)) {
+      return(self[[method_name]](name, col_types, table_name, indexes, full_path, db))
+    } else {
+      return(self$`load_codeset.default`(name, col_types, table_name, indexes, full_path, db))
+    }
+  }
+
+  argos$public_methods$`load_codeset.default` <- function(name,
+                                                          col_types = "iccc",
+                                                          table_name = name,
+                                                          indexes = list("concept_code"),
+                                                          full_path = FALSE,
+                                                          db = self$config("db_src")) {
     if (self$config("cache_enabled")) {
       if (is.null(self$config("_codesets"))) {
         self$config("_codesets", list())
@@ -660,36 +676,17 @@ patch_argos <- function() {
       }
     }
 
-    if (
-      !class(self$config("db_src"))[1] %in%
-        c("Oracle", "src_BigQueryConnection")
-    ) {
-      codes <- self$copy_to_new(
-        db,
-        self$read_codeset(
-          name,
-          col_types = col_types,
-          full_path = full_path
-        ),
-        name = table_name,
-        overwrite = TRUE,
-        indexes = indexes
-      )
-    } else {
-      self$config(
-        "temp_table_drop_me",
-        append(self$config("temp_table_drop_me"), name)
-      )
-
-      codes <- copy_to(
-        dest = db,
-        df = self$read_codeset(name, col_types = col_types, full_path = full_path),
-        name = dbplyr::in_schema(self$config("temp_table_schema"), name),
-        overwrite = TRUE,
-        temporary = FALSE,
-        indexes = indexes
-      )
-    }
+    codes <- self$copy_to_new(
+      db,
+      self$read_codeset(
+        name,
+        col_types = col_types,
+        full_path = full_path
+      ),
+      name = table_name,
+      overwrite = TRUE,
+      indexes = indexes
+    )
 
     if (self$config("cache_enabled")) {
       cache[[name]] <- codes
@@ -699,10 +696,128 @@ patch_argos <- function() {
   }
 
 
+  argos$public_methods$`load_codeset.Oracle` <- function(name,
+                                                         col_types = "iccc",
+                                                         table_name = name,
+                                                         indexes = list("concept_code"),
+                                                         full_path = FALSE,
+                                                         db = self$config("db_src")) {
+    if (self$config("cache_enabled")) {
+      if (is.null(self$config("_codesets"))) {
+        self$config("_codesets", list())
+      }
+      cache <- self$config("_codesets")
+      if (!is.null(cache[[name]])) {
+        return(cache[[name]])
+      }
+    }
+
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        indexes <- NULL
+      }
+    }
+    self$config(
+      "temp_table_drop_me",
+      append(self$config("temp_table_drop_me"), name)
+    )
+
+    codes <- copy_to(
+      dest = db,
+      df = self$read_codeset(name, col_types = col_types, full_path = full_path),
+      name = dbplyr::in_schema(self$config("temp_table_schema"), name),
+      overwrite = TRUE,
+      temporary = FALSE,
+      indexes = indexes
+    )
+
+    if (self$config("cache_enabled")) {
+      cache[[name]] <- codes
+      self$config("_codesets", cache)
+    }
+    codes
+  }
+
+  argos$public_methods$`load_codeset.src_BigQueryConnection` <- function(name,
+                                                                         col_types = "iccc",
+                                                                         table_name = name,
+                                                                         indexes = list("concept_code"),
+                                                                         full_path = FALSE,
+                                                                         db = self$config("db_src")) {
+    if (self$config("cache_enabled")) {
+      if (is.null(self$config("_codesets"))) {
+        self$config("_codesets", list())
+      }
+      cache <- self$config("_codesets")
+      if (!is.null(cache[[name]])) {
+        return(cache[[name]])
+      }
+    }
+
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        indexes <- NULL
+      }
+    }
+    self$config(
+      "temp_table_drop_me",
+      append(self$config("temp_table_drop_me"), name)
+    )
+
+    # Check if we have a separate codeset project specified
+    codeset_project <- self$config("codeset_project")
+    codeset_dataset <- self$config("codeset_dataset_name")
+
+    if (!is.null(codeset_project) && nzchar(codeset_project)) {
+      # Use bigrquery's native functions for more direct control
+      cli::cli_alert_info("Creating codeset table '{name}' in project '{codeset_project}', dataset '{codeset_dataset}'")
+
+      # Create a BigQuery table reference with explicit project and dataset
+      bq_conn <- bigrquery::bq_project_query(codeset_project, "SELECT 1")
+      table_id <- bigrquery::bq_table(project = codeset_project, dataset = codeset_dataset, table = name)
+
+      # Delete the table if it exists (for overwrite)
+      if (bigrquery::bq_table_exists(table_id)) {
+        bigrquery::bq_table_delete(table_id)
+      }
+
+      # Upload data to BigQuery table
+      bigrquery::bq_table_upload(table_id, codeset_data, write_disposition = "WRITE_TRUNCATE")
+
+      # Create a dplyr reference to the table with explicit project.dataset.table format
+      qualified_table_name <- paste0("`", codeset_project, ".", codeset_dataset, ".", name, "`")
+      # Fix for BigQuery syntax - use standard BigQuery syntax without parentheses
+      sql_query <- paste0("SELECT * FROM ", qualified_table_name, " as q")
+      codes <- dplyr::tbl(db, dplyr::sql(sql_query))
+
+      if (self$config("cache_enabled")) {
+        cache[[name]] <- codes
+        self$config("_codesets", cache)
+      }
+      codes
+    }
+  }
+
+
   argos$public_methods$compute_new <- function(tblx,
                                                name = paste0(sample(letters, 12, replace = TRUE), collapse = ""),
                                                temporary = !self$config("retain_intermediates"),
                                                ...) {
+    conn_class <- class(db)[1]
+    method_name <- paste0("compute_new", conn_class)
+
+
+    if (method_name %in% names(self)) {
+      return(self[[method_name]](name, col_types, table_name, indexes, full_path, db))
+    } else {
+      return(self$`load_codeset.default`(name, col_types, table_name, indexes, full_path, db))
+    }
+  }
+
+  argos$public_methods$`compute_new.default` <- function(tblx,
+                                                         name = paste0(sample(letters, 12, replace = TRUE), collapse = ""),
+                                                         temporary = !self$config("retain_intermediates"),
+                                                         ...) {
     if (!inherits(name, c("ident_q", "dbplyr_schema")) && length(name) == 1) {
       name <- gsub("\\s+", "_", name, perl = TRUE)
       name <- self$intermed_name(name, temporary)
@@ -712,15 +827,10 @@ patch_argos <- function() {
     #' to make sure that following db_exists_new and db_remove_new logic doesn't fail.
 
     # Broken in dbplyr, so do it ourselves
-    if (class(self$config("db_src")) == "Microsoft SQL Server" && temporary) {
-      if (self$db_exists_table(con, paste0("#", name))) {
-        self$db_remove_table(con, paste0("#", name))
-      }
-    } else {
-      if (self$db_exists_table(con, name)) {
-        self$db_remove_table(con, name)
-      }
+    if (self$db_exists_table(con, name)) {
+      self$db_remove_table(con, name)
     }
+
     if (self$config("db_trace")) {
       show_query(tblx)
       if (self$config("can_explain")) explain(tblx)
@@ -739,24 +849,8 @@ patch_argos <- function() {
       }
     }
 
-    if (
-      !class(self$config("db_src"))[1] %in%
-        c("Oracle", "src_BigQueryConnection")
-    ) {
-      rslt <-
-        do.call(dplyr::compute, c(list(tblx, name = name, temporary = temporary), ellipsis_args))
-    } else {
-      self$config("temp_table_drop_me", append(self$config("temp_table_drop_me"), name))
-
-      rslt <-
-        do.call(
-          dplyr::copy_to,
-          c(
-            list(con, tblx, name = dbplyr::in_schema(self$config("temp_table_schema"), name), overwrite = TRUE, temporary = FALSE),
-            ellipsis_args
-          )
-        )
-    }
+    rslt <-
+      do.call(dplyr::compute, c(list(tblx, name = name, temporary = temporary), ellipsis_args))
 
     if (self$config("db_trace")) {
       end <- Sys.time()
@@ -766,9 +860,98 @@ patch_argos <- function() {
   }
 
 
-  argos$public_methods$copy_to_new <- function(dest = self$config("db_src"), df, name = deparse(substitute(df)),
-                                               overwrite = TRUE, temporary = !self$config("retain_intermediates"),
+  argos$public_methods$`compute_new.Microsoft SQL Server` <- function(tblx,
+                                                                      name = paste0(sample(letters, 12, replace = TRUE), collapse = ""),
+                                                                      temporary = !self$config("retain_intermediates"),
+                                                                      ...) {
+    if (temporary) {
+      if (self$db_exists_table(con, paste0("#", name))) {
+        self$db_remove_table(con, paste0("#", name))
+      }
+    }
+
+    if (self$config("db_trace")) {
+      show_query(tblx)
+      if (self$config("can_explain")) explain(tblx)
+      message(
+        " -> ",
+        base::ifelse(packageVersion("dbplyr") < "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(name, con))
+      )
+      start <- Sys.time()
+      message(start)
+    }
+
+    ellipsis_args <- list(...)
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        ellipsis_args$indexes <- c()
+      }
+    }
+
+    rslt <- do.call(dplyr::compute, c(list(tblx, name = name, temporary = temporary), ellipsis_args))
+
+    if (self$config("db_trace")) {
+      end <- Sys.time()
+      message(end, " ==> ", format(end - start))
+    }
+    rslt
+  }
+
+  argos$public_methods$`compute_new.Oracle` <- function(tblx,
+                                                        name = paste0(sample(letters, 12, replace = TRUE), collapse = ""),
+                                                        temporary = !self$config("retain_intermediates"),
+                                                        ...) {
+    if (self$config("db_trace")) {
+      show_query(tblx)
+      if (self$config("can_explain")) explain(tblx)
+      message(
+        " -> ",
+        base::ifelse(packageVersion("dbplyr") < "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(name, con))
+      )
+      start <- Sys.time()
+      message(start)
+    }
+
+    ellipsis_args <- list(...)
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        ellipsis_args$indexes <- c()
+      }
+    }
+
+    self$config("temp_table_drop_me", append(self$config("temp_table_drop_me"), name))
+
+    rslt <- do.call(dplyr::copy_to, c(list(con, tblx, name = dbplyr::in_schema(self$config("temp_table_schema"), name), overwrite = TRUE, temporary = FALSE), ellipsis_args))
+
+    if (self$config("db_trace")) {
+      end <- Sys.time()
+      message(end, " ==> ", format(end - start))
+    }
+    rslt
+  }
+
+  argos$public_methods$copy_to_new <- function(dest = self$config("db_src"), df,
+                                               name = deparse(substitute(df)),
+                                               overwrite = TRUE,
+                                               temporary = !self$config("retain_intermediates"),
                                                ...) {
+    conn_class <- class(db)[1]
+    method_name <- paste0("copy_to_new", conn_class)
+
+
+    if (method_name %in% names(self)) {
+      return(self[[method_name]](name, col_types, table_name, indexes, full_path, db))
+    } else {
+      return(self$`load_codeset.default`(name, col_types, table_name, indexes, full_path, db))
+    }
+  }
+
+
+  argos$public_methods$`copy_to_new.default` <- function(dest = self$config("db_src"), df,
+                                                         name = deparse(substitute(df)),
+                                                         overwrite = TRUE,
+                                                         temporary = !self$config("retain_intermediates"),
+                                                         ...) {
     name <- self$intermed_name(name, temporary = temporary)
     if (self$config("db_trace")) {
       message(" -> copy_to")
@@ -776,10 +959,10 @@ patch_argos <- function() {
       message(start)
       message("Data: ", deparse(substitute(df)))
       message("Table name: ", base::ifelse(packageVersion("dbplyr") <
-        "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(
-        name,
-        dbi_con(dest)
-      )), " (temp: ", temporary, ")")
+                                             "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(
+                                               name,
+                                               dbi_con(dest)
+                                             )), " (temp: ", temporary, ")")
       message("Data elements: ", paste(tbl_vars(df), collapse = ","))
       message("Rows: ", NROW(df))
     }
@@ -800,36 +983,116 @@ patch_argos <- function() {
         ellipsis_args$indexes <- c()
       }
     }
-    if (
-      !class(self$config("db_src"))[1] %in%
-        c("Oracle", "src_BigQueryConnection")
-    ) {
-      rslt <- do.call(dplyr::copy_to, (c(
-        list(dest = dest, df = df, name = name, overwrite = overwrite, temporary = temporary), ellipsis_args
-      )))
-    } else {
-      self$config(
-        "temp_table_drop_me",
-        append(self$config("temp_table_drop_me"), name)
-      )
 
-      rslt <-
-        do.call(
-          dplyr::copy_to,
-          c(
-            list(dest, df,
-              name = dbplyr::in_schema(self$config("temp_table_schema"), name), overwrite = TRUE, temporary = FALSE
-            ),
-            ellipsis_args
-          )
-        )
-    }
+    rslt <- do.call(dplyr::copy_to, (c(list(dest = dest, df = df, name = name, overwrite = overwrite, temporary = temporary), ellipsis_args)))
+
     if (self$config("db_trace")) {
       end <- Sys.time()
       message(end, " ==> ", format(end - start))
     }
     rslt
   }
+
+  argos$public_methods$`copy_to_new.Microsoft SQL Server` <- function(dest = self$config("db_src"), df,
+                                                                      name = deparse(substitute(df)),
+                                                                      overwrite = TRUE,
+                                                                      temporary = !self$config("retain_intermediates"),
+                                                                      ...) {
+    name <- self$intermed_name(name, temporary = temporary)
+    if (self$config("db_trace")) {
+      message(" -> copy_to")
+      start <- Sys.time()
+      message(start)
+      message("Data: ", deparse(substitute(df)))
+      message("Table name: ", base::ifelse(packageVersion("dbplyr") <
+                                             "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(
+                                               name,
+                                               dbi_con(dest)
+                                             )), " (temp: ", temporary, ")")
+      message("Data elements: ", paste(tbl_vars(df), collapse = ","))
+      message("Rows: ", NROW(df))
+    }
+
+    if (temporary) {
+      if (overwrite && self$db_exists_table(dest, paste0("#", name))) {
+        self$db_remove_table(dest, paste0("#", name))
+      }
+    } else {
+      if (overwrite && self$db_exists_table(dest, name)) {
+        self$db_remove_table(dest, name)
+      }
+    }
+
+
+    ellipsis_args <- list(...)
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        ellipsis_args$indexes <- c()
+      }
+    }
+
+    rslt <- do.call(dplyr::copy_to, (c(list(dest = dest, df = df, name = name, overwrite = overwrite, temporary = temporary), ellipsis_args)))
+
+
+    if (self$config("db_trace")) {
+      end <- Sys.time()
+      message(end, " ==> ", format(end - start))
+    }
+    rslt
+  }
+
+
+  argos$public_methods$`copy_to_new.Oracle` <- function(dest = self$config("db_src"), df,
+                                                        name = deparse(substitute(df)),
+                                                        overwrite = TRUE,
+                                                        temporary = !self$config("retain_intermediates"),
+                                                        ...) {
+    name <- self$intermed_name(name, temporary = temporary)
+    if (self$config("db_trace")) {
+      message(" -> copy_to")
+      start <- Sys.time()
+      message(start)
+      message("Data: ", deparse(substitute(df)))
+      message("Table name: ", base::ifelse(packageVersion("dbplyr") <
+                                             "2.0.0", dbplyr::as.sql(name), dbplyr::as.sql(
+                                               name,
+                                               dbi_con(dest)
+                                             )), " (temp: ", temporary, ")")
+      message("Data elements: ", paste(tbl_vars(df), collapse = ","))
+      message("Rows: ", NROW(df))
+    }
+
+    if (overwrite && self$db_exists_table(dest, name)) {
+      self$db_remove_table(dest, name)
+    }
+
+
+    ellipsis_args <- list(...)
+    if (self$config_exists("can_index")) {
+      if (!self$config("can_index")) {
+        ellipsis_args$indexes <- c()
+      }
+    }
+
+    self$config(
+      "temp_table_drop_me",
+      append(self$config("temp_table_drop_me"), name)
+    )
+
+    rslt <-
+      do.call(
+        dplyr::copy_to,
+        c(list(dest, df, name = dbplyr::in_schema(self$config("temp_table_schema"), name), overwrite = TRUE, temporary = FALSE), ellipsis_args)
+      )
+
+
+    if (self$config("db_trace")) {
+      end <- Sys.time()
+      message(end, " ==> ", format(end - start))
+    }
+    rslt
+  }
+
 
   argos$private_methods$.ora_env_setup <-
     function(db) {
@@ -885,8 +1148,6 @@ patch_argos <- function() {
       name
     }
 
-
-
   #' Correct Table Column Names to Lowercase
   #'
   #' This function checks the column names of a data frame or tibble and converts them to lowercase if any are not already.
@@ -915,6 +1176,7 @@ patch_argos <- function() {
     self$tbl_case_corrector(self$qual_tbl(name, "cdm_schema", db))
   }
 }
+
 
 
 pkgLoad <- function() {
