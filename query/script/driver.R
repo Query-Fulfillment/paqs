5499ed99448089709c25fe1166175a17b8170de2
Sahal Master <sahalmaster@outlook.com>
2025-08-20 12:29:57 -0400

    Cleaning driver.R

--- query/script/driver.R
+++ query/script/driver.R
@@ -39,60 +39,7 @@ run <- function() {
 
   codesets <- load_all_codesets()
   
-  # Diabetes
   
-  # Atleast one occurrence of T2 Diabetes code between 2021 - 2024
-  rslt$dx_t2_diabetes <- define_criteria(codeset = codesets$dx_t2_diabetes,
-                                         start_date = "01-01-2015",
-                                         end_date = "12-31-2024",
-                                         min_codes_required = 1,
-                                         min_days_separation = 0,
-                                         qualifying_event = 'all',
-                                         criterion_suffix = "dx_t2_diabetes") 
-  
-  # Atleast one occurrence of T2 Diabetes code between diagnosis date and 2024
-  rslt$dx_t2_any_anti_diabetic_post_dx <- define_criteria(cohort = rslt$dx_t2_diabetes,
-                                                  codeset = codesets$rx_any_anti_diabetic,
-                                                  start_date = "criterion_dx_t2_diabetes_date",
-                                                  end_date = "12-31-2024",
-                                                  min_codes_required = 1,
-                                                  min_days_separation = 0,
-                                                  qualifying_event = 'first',
-                                                  criterion_suffix = "any_anti_diabetic_post")
-  
-  rslt$dx_t2_any_anti_diabetic_pre_dx <- define_criteria(cohort = rslt$dx_t2_diabetes,
-                                                  codeset = codesets$rx_any_anti_diabetic,
-                                                  start_date = "01-01-2015",
-                                                  end_date = "criterion_dx_t2_diabetes_date",
-                                                  min_codes_required = 1,
-                                                  min_days_separation = 0,
-                                                  qualifying_event = 'first',
-                                                  criterion_suffix = "any_anti_diabetic_pre")
-
-  # Filtering patients who had an anti-diabetic medication exposure prior to their T2D diagnosis
-  
-  rslt$dx_t2_any_anti_diabetic_post_dx_no_prior <- rslt$dx_t2_any_anti_diabetic_post_dx %>% 
-    anti_join(rslt$dx_t2_any_anti_diabetic_pre_dx, by = c('patid'))
-
-   # Atleast one occurrence of T2 Diabetes code between diagnosis date and end of query period
-  rslt$post_any_adb_any_glp <- define_criteria(cohort =   rslt$dx_t2_any_anti_diabetic_post_dx_no_prior,
-                                               codeset = codesets$rx_any_glp,
-                                               start_date = "criterion_any_anti_diabetic_post_date",
-                                               end_date = "12-31-2024",
-                                               min_codes_required = 1,
-                                               min_days_separation = 0,
-                                               qualifying_event = 'first',
-                                               criterion_suffix = "any_glp")
-   
-  # Patients with a t2dm dx code and anti-diabetic medication + any glp 2 + bariatric surgery post dx date
-    rslt$tdx_with_bariatric_surgery <- define_criteria(cohort = rslt$dx_t2_diabetes %>% inner_join(rslt$post_any_adb_any_glp %>% distinct(patid)),
-                                                       codeset = codesets$px_bariatric_surgery,
-                                                       start_date = "01-01-2015",
-                                                       end_date = "12-31-2024",
-                                                       min_codes_required = 1,
-                                                       min_days_separation = 0,
-                                                       qualifying_event = 'first',
-                                                       criterion_suffix = "bariatric_surgery")
 
   # ===================================================================================================
   #' **Standard code DO NOT EDIT**
