0380948f010668ff4f29944e2994aca5902719a7
Marchesani, Nicole C <MARCHESANN@chop.edu>
2026-01-06 11:28:40 -0500

    ignore codesets that are mapping files

--- query/script/base/codesets.R
+++ query/script/base/codesets.R
@@ -1,12 +1,15 @@
 load_all_codesets <- function(path = paste0('./query/',get_argos_default()$config('subdirs')$spec_dir)) {
-
+  
   files <- gsub(".csv","",list.files(path, pattern = 'csv'))
-
+  
+  exclude_files <- c("ses_map", "ruca_map", "state_map", "cohort_names_map","crosswalks")
+  files <- setdiff(files, exclude_files)
+  
   codesets <- setNames(
-  lapply(files, load_codeset, col_types = "ccc", indexes = "code"),
-  basename(files)
+    lapply(files, load_codeset, col_types = "ccc", indexes = "code"),
+    basename(files)
   )
-
-codesets$crosswalk <- match_codetype_to_table()
-codesets
+  
+  codesets[["crosswalk"]] <- load_codeset('crosswalks', 'ccc', indexes = NULL)
+  codesets
 }
